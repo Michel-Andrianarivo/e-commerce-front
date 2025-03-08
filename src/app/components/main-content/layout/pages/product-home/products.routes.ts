c318df2ab5a3871f960cd362b832caf41b3bd617
Elxiim <michelandrianarivoo@gmail.com>
2025-03-08 19:45:32 +0300

    adding new routes

--- src/app/components/main-content/layout/pages/product-home/products.routes.ts
+++ src/app/components/main-content/layout/pages/product-home/products.routes.ts
@@ -20,6 +20,27 @@ export const routes: Routes = [
             (c) => c.BagsComponent
           ),
       },
+      {
+        path: 'cosmetics',
+        loadComponent: () =>
+          import('../product-home/cosmetics/cosmetics.component').then(
+            (c) => c.CosmeticsComponent
+          ),
+      },
+      {
+        path: 'dress',
+        loadComponent: () =>
+          import('../product-home/dress/dress.component').then(
+            (c) => c.DressComponent
+          ),
+      },
+      {
+        path: 'accessories',
+        loadComponent: () =>
+          import('../product-home/accessories/accessories.component').then(
+            (c) => c.AccessoriesComponent
+          ),
+      },
     ],
   },
 ];
