0c1f6f3451c613e4ee77fefba9e172ed4b791d2e
Elxiim <michelandrianarivoo@gmail.com>
2025-03-11 21:13:25 +0300

    bags refonte

--- src/app/components/main-content/layout/pages/product-home/bags/bags.component.ts
+++ src/app/components/main-content/layout/pages/product-home/bags/bags.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-bags',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './bags.component.html',
   styleUrl: './bags.component.scss',
 })
